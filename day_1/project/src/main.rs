e9026f31bcb8847f823c459cea33c9e126abe6a6
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-25 12:39:32 +0500

    Add: initail day one

---
+++ day_1/project/src/main.rs
@@ -0,0 +1,44 @@
+
+
+fn main() {
+    println!("Data Types!!");
+
+    let num:u8 = 8;
+    println!("num: {num}");
+
+
+    // &str!
+    let string_liternal = "Hi Fodetrs!";
+    println!("string_liternal: {string_liternal}");
+}
+
+// SNAKE CASE:-  IS ALLOWED IN RUST LANGUAGE!
+// / CAMEL CASE:-  IS NOT ALLOWED IN RUST!
+// SEMI COLON:-  IS USED TO TERMINATE THE STATEMENT!
+// CARGO: ITS OUR PACKAGE MANAGER!
+// Cargo.toml: ITS OUR PROJECT MANIFEST FILE!
+// Cargo.lock: ITS OUR PROJECT LOCK FILE!
+// TOM OBIOUS MINIMUL LANGUAGE!
+// CARGO BUILD:- IT IS USED TO BUILD THE PROJECT( compile to machine code in binary executable code!)!
+// CARGO RUN:-  IT IS USED TO RUN THE PROJECT!
+// CARGO TEST:-  IT IS USED TO RUN THE TEST!
+// CARGO CHECK:-  IT IS USED TO RUN THE CHECK!
+
+ 
+// DATA TYPES:
+// INTEGERS!
+// SCALAR && COMPOUND TYPES!
+// SCALAR TYPES:
+// INTEGERS!
+// FLOATS!
+// BOOLEANS!
+// CHARACTERS!
+// COMPOUND TYPES:
+// TUPLES!
+// ARRAYS!
+// STRINGS!
+
+// STRING:
+// They are stored in heap and they can extends their sizes!
+// &str:
+// They cannot change their size and are not dynamic and they are stored in stack (not stored in heap)!
