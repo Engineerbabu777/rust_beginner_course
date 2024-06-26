714aebdefde184f19e466e3e4b813db62774d614
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-26 22:08:40 +0500

    Add: tupels day 2 learning

---
+++ day_2/project/src/main.rs
@@ -0,0 +1,26 @@
+fn main() {
+    println!("Tupels :)");
+
+    let emp_info:(&str,u8) = ("Ramish",50);
+
+
+    println!("{}",emp_info.0);
+    println!("{}",emp_info.1);
+
+    // desctructing!
+    let (name,age) = emp_info;
+
+    println!("{}",name);
+    println!("{}",age);
+}
+
+
+// tupples!!
+// tupples are immutable by default
+// tupples are fixed size
+// tupples can have multiple types
+// tupples can have multiple values
+// tupples can be destructured
+// tupples can be used as function arguments
+// tupples can be returned from functions
+// tupples can be used in match expressions
---
+++ day_2/project/target/debug/deps/project.d
@@ -0,0 +1,5 @@
+D:\2024 Plan\rust_course_begginer\day_2\project\target\debug\deps\project.exe: src\main.rs
+
+D:\2024 Plan\rust_course_begginer\day_2\project\target\debug\deps\project.d: src\main.rs
+
+src\main.rs:
