3424a74376b03fe62ccdc5918960d42063e16bb2
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-28 11:22:45 +0500

    Add: test

---
+++ day_2/project/target/debug/deps/project-be4f11a3eece09b3.d
@@ -0,0 +1,5 @@
+d:\2024 Plan\rust_course_begginer\day_2\project\target\debug\deps\project-be4f11a3eece09b3.exe: src\main.rs
+
+d:\2024 Plan\rust_course_begginer\day_2\project\target\debug\deps\project-be4f11a3eece09b3.d: src\main.rs
+
+src\main.rs:
